b8842175f842197289ba9271be1ea427e275f73d
liara987 <987.liara@gmail.com>
2023-01-19 16:06:16 -0300

    feature: Cria componente JobList

---
+++ elo7-desafio/src/components/JobList/documentation.md
@@ -0,0 +1,3 @@
+# JobList
+
+O JobList é um componente que faz uma requisição na [API](http://www.mocky.io/v2/5d6fb6b1310000f89166087b) e lista as vagas, validando se a vaga é ainda esta ativa e se é na modalidade remota.
---
+++ elo7-desafio/src/components/JobList/index.jsx
@@ -0,0 +1,57 @@
+import "./styles.scss";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function JobList() {
+  const [jobs, setJobs] = useState([]);
+  const baseURL = "http://www.mocky.io/v2/5d6fb6b1310000f89166087b";
+
+  useEffect(() => {
+    axios
+      .get(baseURL)
+      .then((response) => {
+        let items = [];
+        response.data.vagas.map((item, index) => {
+          items.push({
+            id: index,
+            role: item.cargo,
+            active: item.ativa,
+            link: item.link,
+            remote: item.localizacao ? false : true,
+            adress:
+              item.localizacao?.bairro +
+              " - " +
+              item.localizacao?.cidade +
+              ", " +
+              item.localizacao?.pais,
+          });
+        });
+        setJobs(items);
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
+  return (
+    <div className="job-list">
+      <h3 className="job-list-title">desenvolvimento</h3>
+      <ul className="job-list-wrapper">
+        {jobs.map((job) => {
+          if (job.active) {
+            return (
+              <li className="job-list-item" key={job.id}>
+                <span className="job-list-item-role">{job.role}</span>
+                <span className="job-list-item-neighborhood">
+                  {job.remote ? "Remoto" : job.adress}
+                </span>
+              </li>
+            );
+          }
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default JobList;
